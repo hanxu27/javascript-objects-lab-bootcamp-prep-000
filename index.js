6e0ce68d72037162b1ea111e9a1c205e23e645c5
han xu <hanxu27@gmail.com>
2019-03-01 01:56:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return newObject
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object = Object.assign({}, object, {key: value})
+  return object
+}
