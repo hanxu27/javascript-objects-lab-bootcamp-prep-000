2eb03d7493e7a492976bbf2012766c2b97dff6c3
han xu <hanxu27@gmail.com>
2019-03-01 01:54:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var recipes = {}
+var recipes = { food: 'bar'}
 
 function updateObjectWithKeyAndValue(object, key, value) {
   object = Object.assign({}, object, {key: value})
   return object
 }
+
